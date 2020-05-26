1e9852b2922b78e1ff42c68a9586fb0de51ca2db
ss <sssscorejava@gmail.com>
2020-05-27 00:59:48 +0100

    feat: Add redux store to application
    Created new redux store by using createStore() of redux and passed root reducer and middlewares to it.
    
    Surrounded the App component with Provider component provided by 'react-redux' and passed store as prop to it. It will enabled application now to brodcast store to all components which menas we will not have to send data as props to nestedeeply nested components.
    
    Components at the moment are not using this store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import reducers from './reducers';
+import middlewares from './middleware';
+
+
+const store = createStore(reducers, middlewares);
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
