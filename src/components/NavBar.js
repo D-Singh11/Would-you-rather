855340b53178adf675675ace048e3d8c75597e56
ss <sssscorejava@gmail.com>
2020-05-27 02:15:32 +0100

    feat: Create new components folder and build NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom'
+
+class Nav extends Component {
+    render() {
+        return (
+            <nav className='nav'>
+                <ul>
+                    <li>
+                        <NavLink to='/' exact activeClassName='active'>
+                            Homepage
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/newQuestion' activeClassName='active'>
+                            Add question
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/leaderboard' activeClassName='active'>
+                            Leaderboard
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/login' activeClassName='active'>
+                            Login
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        )
+    }
+}
+
+export default Nav
