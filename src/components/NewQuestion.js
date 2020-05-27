3c31a0b7ee5815d9e802f79d6c5cd8997314b993
ss <sssscorejava@gmail.com>
2020-05-27 20:59:51 +0100

    feat: Add Manin components of application
    Added the all the application components with placeholder views which will be built one by one

---
+++ src/components/NewQuestion.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class NewQuestion extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Add new question</h1>
+            </div>
+        )
+    }
+}
+
+export default NewQuestion
---
+++ src/components/PollDetail.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class PollDetail extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Poll Details</h1>
+            </div>
+        )
+    }
+}
+
+export default PollDetail
