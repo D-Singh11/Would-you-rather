369d3431d4e564b09791848bf67a7e3a637fc7cd
ss <sssscorejava@gmail.com>
2020-05-28 14:52:05 +0100

    feat: Build UIform of NewQuestion component

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -3,8 +3,18 @@ import React, { Component } from 'react'
 class NewQuestion extends Component {
     render() {
         return (
-            <div>
+            <div className='newQuestion'>
                 <h1>Add new question</h1>
+                <form action="" className='center'>
+                    <label htmlFor="optionOne">Option one</label>
+                    <input type="text" />
+
+                    <label htmlFor="optionTwo">Option two</label>
+                    <input type="text" />
+                    <button type="submit" className='btn-large amber center'>
+                        Save
+                        </button>
+                </form>
             </div>
         )
     }
