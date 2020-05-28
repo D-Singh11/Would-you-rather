764d7b3685291e4736c0d657f66bfc9234f38396
ss <sssscorejava@gmail.com>
2020-05-28 01:51:48 +0100

    feat: Build Poll form and connect the Poll component to store
    It is also an controlled component because it handle the state of form using local state.
    Added onChange eventlistener to redip buttons on the page.
    Displayed correct options for form using data from state

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -1,13 +1,77 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 
 class Poll extends Component {
+
+    state = {
+        text: ''
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            text: event.target.value
+        });
+        console.log(event.target.value);
+    }
     render() {
+        const { author, optionOne, id, optionTwo } = this.props;
         return (
-            <div>
-                <h1>Poll page</h1>
+            <div className="card">
+                <div className="card-content center">
+                    <div className='center'>
+                        <img src="https://lorempixel.com/100/190/nature/6" alt='avatar of user' className='circle' />
+                        <p className='card-title'>{author} asks</p>
+                    </div>
+                    <h5 className="header">Would you rather?</h5>
+                    <form onSubmit={this.handleSubmit}>
+                        <p>
+                            <label>
+                                <input className="with-gap"
+                                    name="group1"
+                                    type="radio"
+                                    value={optionOne.text}
+                                    onChange={this.handleChange}
+                                />
+                                <span className='text-black'>{optionOne.text}</span>
+                            </label>
+                        </p>
+                        <p>
+                            <label>
+                                <input className="with-gap"
+                                    name="group1"
+                                    type="radio"
+                                    value={optionTwo.text}
+                                    onChange={this.handleChange}
+                                />
+                                <span>{this.props.optionTwo.text}</span>
+                            </label>
+                        </p>
+                        <button className='btn-large amber lighten-2'>
+                            Submit
+                        </button>
+                    </form>
+                </div>
+                {/* <Link to={`/detail/${id}`} className='white-text'>
+                    <div className='card-action amber lighten-2'>
+                        <p className='center '>
+                            <b>View Detail</b>
+                        </p>
+                    </div>
+                </Link> */}
             </div>
         )
     }
 }
 
-export default Poll
+function mapStateToProps({ users, questions, authedUser }, props) {
+    const author = users[authedUser].name;
+    const question = questions[props.match.params.id];
+
+    return {
+        ...question,
+        author
+    }
+}
+
+export default connect(mapStateToProps)(Poll);
