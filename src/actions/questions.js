682082594d1145a063bc3911afdac9d5f2c31c61
ss <sssscorejava@gmail.com>
2020-05-28 14:39:30 +0100

    feat: Add new action creators to Add ne question and update questions reducer to update state in store
    Created thunked action creator handleAddQuestion() which saves the new question in backend first and then only dispatch() action to updat ethe store state

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,8 +1,9 @@
-import { saveAnswer } from '../utils/api';
+import { saveAnswer, saveQuestion } from '../utils/api';
 
 
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS';
 export const SAVE_ANSWER = 'SAVE_ANSWER';
+export const ADD_QUESTION = 'ADD_QUESTION';
 
 export function setQuestionsAction(questions) {
     return {
@@ -18,17 +19,32 @@ function saveAnswerAction(question) {
     }
 }
 
+function addQuestionAction(question) {
+    return {
+        type: ADD_QUESTION,
+        question
+    }
+}
+
 export function handleSaveAnswer(qid, answer) {
     return (dispatch, getState) => {
         const authedUser = getState().authedUser;
-        dispatch(saveAnswerAction({authedUser,qid, answer}));
+        dispatch(saveAnswerAction({ authedUser, qid, answer }));
         console.log("State", getState());
         return saveAnswer({
             authedUser,
             qid,
             answer
-        }).catch(error =>{
+        }).catch(error => {
             alert('Error - Answer not saved Try again');
         });
+    }
+}
+
+export function handleAddQuestion(question) {
+    return (dispatch) => {
+        return saveQuestion(question).then(question => {
+            dispatch(saveQuestion(question));
+        })
     }
 }
