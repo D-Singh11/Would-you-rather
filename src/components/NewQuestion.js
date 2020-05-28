da99a5e5a009aa08ac44fb8dbf7bc4a6cca897cb
ss <sssscorejava@gmail.com>
2020-05-28 15:17:44 +0100

    feat: Add handleSubmit() to handle the operation on form submission
    Added event listener handleSubmit() which will dispatch() an thunked action to add new question to API and redux store.
    
    Added placeholder text to inputs.
    Used 'ref's to read the form input value instead of usong onChange event

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react'
 
 class NewQuestion extends Component {
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        if(this.optionOne.value.trim() && this.optionTwo.value.trim()){
+            return alert("Dispatch add question action");
+        }
+        alert("Provide both options to save new question");
+    }
+
     render() {
         return (
             <div className='newQuestion'>
                 <h1>Add new question</h1>
-                <form action="" className='center'>
+                <form onSubmit={this.handleSubmit} className='center'>
                     <label htmlFor="optionOne">Option one</label>
-                    <input type="text" />
+                    <input type="text"
+                        placeholder='add first option'
+                        ref={(optionOne) => this.optionOne = optionOne}
+                    />
 
                     <label htmlFor="optionTwo">Option two</label>
-                    <input type="text" />
+                    <input type="text"
+                        placeholder='add second option '
+                        ref={(optionTwo) => this.optionTwo = optionTwo}
+                    />
                     <button type="submit" className='btn-large amber center'>
                         Save
                         </button>
