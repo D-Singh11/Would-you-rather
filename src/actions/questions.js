b706ab3c877a32bf503a35e12226294bc687ee78
ss <sssscorejava@gmail.com>
2020-05-28 13:53:20 +0100

    feat: Create action creator and update reducer to save answer of question
    Created thunked action  reator handleSaveAnswer() to save anser to backend and Update state in store

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,8 +1,34 @@
-export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS';
+import { saveAnswer } from '../utils/api';
 
-export function setQuestionsAction(questions){
+
+export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS';
+export const SAVE_ANSWER = 'SAVE_ANSWER';
+
+export function setQuestionsAction(questions) {
     return {
         type: RECEIVE_QUESTIONS,
         questions
-    }   
+    }
+}
+
+function saveAnswerAction(question) {
+    return {
+        type: SAVE_ANSWER,
+        question
+    }
+}
+
+export function handleSaveAnswer(qid, answer) {
+    return (dispatch, getState) => {
+        const authedUser = getState().authedUser;
+        dispatch(saveAnswerAction({authedUser,qid, answer}));
+        console.log("State", getState());
+        return saveAnswer({
+            authedUser,
+            qid,
+            answer
+        }).catch(error =>{
+            alert('Error - Answer not saved Try again');
+        });
+    }
 }
--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,4 +1,4 @@
-import { RECEIVE_QUESTIONS } from '../actions/questions';
+import { RECEIVE_QUESTIONS, SAVE_ANSWER } from '../actions/questions';
 
 export default function questions(state = {}, action) {
     switch (action.type) {
@@ -8,6 +8,18 @@ export default function questions(state = {}, action) {
                 ...action.questions
             }
 
+        case SAVE_ANSWER:
+            return {
+                ...state,
+                [action.question.qid]: {
+                    ...state[action.question.qid],
+                    [action.question.answer]: {
+                        ...state[action.question.qid][action.question.answer],
+                        votes: state[action.question.qid][action.question.answer].votes.concat(action.question.authedUser)
+                    }
+                }
+
+            }
         default:
             return state;
     }
