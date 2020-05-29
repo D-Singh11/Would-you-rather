88fe1ffc4094b6cd38df1f8fc268b4a7ff72b329
ss <sssscorejava@gmail.com>
2020-05-30 00:43:17 +0100

    feat: Build Login page UI and apply login functionality
    Created the login use which allow site visitor to select from the existing users which then log them in as selected user.
    Connected the component to store using connect() of react-redux.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom'
+
+
+import { setAuthedUserAction } from '../actions/authedUser';
 
 class Login extends Component {
+
+    handleChange = (event) => {
+        event.preventDefault();
+        console.log(event.target.value);
+        // this.props.dispatch(handleInitialData());
+        this.props.dispatch(setAuthedUserAction(event.target.value));
+        // this.props.history.push('/');
+    }
+
     render() {
         return (
-            <div>
+            <div className='row'>
                 <h1>Login page</h1>
+                <div className="col s6">
+                    <label className='text-blue'>Login as :</label>
+                    <select onChange={this.handleChange} className='browser-default'>
+                        <option value="" defaultValue>Choose your username</option>
+                        <option value="sarahedo"
+                            data-icon="https://tylermcginnis.com/would-you-rather/sarah.jpg">
+                            Sarah edo
+                            </option>
+                        <option value="tylermcginnis"
+                            data-icon="https://tylermcginnis.com/would-you-rather/tyler.jpg">
+                            Tyler Mcginnis
+                            </option>
+                        <option value="johndoe"
+                            data-icon="https://tylermcginnis.com/would-you-rather/dan.jpg">
+                            John Doe
+                            </option>
+                    </select>
+                    <button className="btn-large waves-effect waves-light right" type="submit" name="action">
+                        Login
+                    </button>
+                </div>
             </div>
         )
     }
 }
 
-export default Login
+export default connect()(Login);
