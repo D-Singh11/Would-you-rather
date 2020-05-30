ca75e6cfecb8136c1b69bc61cbe394cc984766bf
ss <sssscorejava@gmail.com>
2020-05-30 15:10:07 +0100

    fix: Change tags attribute from 'class' to 'className'

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -6,14 +6,14 @@ class PageNotFound extends Component {
         return (
             <div className="container">
                 <h3>Page not found</h3>
-                <div class="card  z-depth-5 center">
-                    <div class="card-action center">
+                <div className="card  z-depth-5 center">
+                    <div className="card-action center">
                         <Link to='/' className='btn'>Go back</Link>
                     </div>
-                    <div class="card-image">
+                    <div className="card-image">
                         <img src="https://media.giphy.com/media/UoeaPqYrimha6rdTFV/giphy.gif" alt='page not found'/>
                     </div>
-                    <div class="card-content">
+                    <div className="card-content">
                         <span>Page you tried to access does not exist. It could be because of incorrect URL</span>
                     </div>
                 </div>
