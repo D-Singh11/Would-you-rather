30722e561dab852c20e100455acc1ec3cb515ff9
ss <sssscorejava@gmail.com>
2020-05-30 04:00:51 +0100

    feat: Add new action creator to Save the answer to user profile
    It was missed previously when save answer was implemented.
    Previous implementation only included saving question's answer to questions state but
    it should of also added the id of answered question to user.answers object along with option choosen.
    
    Creator new action type and creator to handle this operation.
    Also added one more case to update the users state in the store correctly

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,8 +1,16 @@
 export const RECEIVE_USERS = 'RECEIVE_USERS';
+export const SAVE_USER_ANSWER = 'SAVE_USER_ANSWER';
 
 export function setUsersAction(users){
     return {
         type: RECEIVE_USERS,
         users
+    }
+}
+
+export function saveUserAnswerAction(question) {
+    return {
+        type: SAVE_USER_ANSWER,
+        question
     }
 }
