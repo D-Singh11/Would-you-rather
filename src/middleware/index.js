b7f4398d0fb6851003b131e8df75d248d8bea9d2
ss <sssscorejava@gmail.com>
2020-05-31 23:14:12 +0100

    docs:Add comments for index.js/root middlewares file

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -2,6 +2,13 @@ import thunk from 'redux-thunk';
 import { applyMiddleware } from 'redux';
 import logger from './logger';
 
+ /**
+  * @description It is used to create a root middleware by combining all other
+  * middleware functions using applyMiddleware() function provided by 'redux library.
+  * @param {functon} thunk
+  * @param {function} logger 
+  * @return {function} rootMiddleware 
+  */
 export default applyMiddleware(
     thunk,
     logger
