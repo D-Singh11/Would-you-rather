904d3f78ce17cb1718ccc3c7a2f275fc1aa11851
ss <sssscorejava@gmail.com>
2020-05-31 22:57:12 +0100

    docs: Add comment sto logger middleware file

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,3 +1,9 @@
+/**
+* @description Custom middleware called logger used to log the action type, 
+* and state of store before and after the action is complete.
+* @param {object} store
+* @returns {function} 
+*/
 const logger = (store) => (next) => (action) => {
     console.group(action.type);
         console.log('State before action', store.getState());
